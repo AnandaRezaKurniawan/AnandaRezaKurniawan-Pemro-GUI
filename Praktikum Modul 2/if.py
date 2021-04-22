e55f2658b7490b672940790ffdd9f13a87910542
ananda19104061 <72422050+ananda19104061@users.noreply.github.com>
2021-04-22 21:45:59 +0700

    Create if.py

---
+++ Praktikum Modul 2/if.py
@@ -0,0 +1,26 @@
+# 19104061
+x = False
+y = True
+
+# if.. else
+if x != True:
+    print('print')
+else:
+    print('tidak print')
+
+print('\n')
+
+# if elif
+if x != False:
+    print('print')
+elif y != False:
+    print('tidak print')
+
+print('\n')
+
+
+#if (and) else
+if x != True and y == False:
+    print('print')
+else:
+    print('tidak print')
