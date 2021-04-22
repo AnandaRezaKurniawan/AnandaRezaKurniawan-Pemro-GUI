ee9d31f935b3c74d309068b8fa09fc6223dadefb
ananda19104061 <72422050+ananda19104061@users.noreply.github.com>
2021-04-22 21:40:30 +0700

    membuat fungsi.py

---
+++ Praktikum Modul 2/fungsi.py
@@ -0,0 +1,39 @@
+# fungsi global di python
+
+def kali (a,b):
+    c = a * b
+    return c
+
+z = kali(10,5)
+print(z)
+
+# fungsi globals
+def cetakBonus(daftar=[]):
+    # fungsi lokal
+    def hitungBonus(gaji):
+        if gaji < 5000000 :
+            bonus = 0.05 * gaji
+        elif 5000000 <= gaji < 7500000:
+            bonus = 0.10 * gaji
+        else:
+            bonus = 0.15 * gaji
+        return bonus
+
+    for nama, gaji in daftar:
+        b = hitungBonus(gaji)
+        print(f'{nama}\t: {gaji}, {b} ')
+
+data = [
+    ('ucok', 4000000),
+    ('budi', 6000000),
+    ('wagi', 7000000)
+]
+
+cetakBonus(data)
+
+# fungsi lambda
+maks = lambda a, b : a if a > b else b
+print(maks (25, 20))
+
+same = lambda a : True if a == 25 else False
+print(same (20))
