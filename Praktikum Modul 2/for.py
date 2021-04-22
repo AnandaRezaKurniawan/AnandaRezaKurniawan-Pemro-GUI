60f03b5b8ae9ea7501af9c1f684b7e07d71305ca
ananda19104061 <72422050+ananda19104061@users.noreply.github.com>
2021-04-22 21:38:47 +0700

    membuat for.py

---
+++ Praktikum Modul 2/for.py
@@ -0,0 +1,22 @@
+# Bahasa python
+angka = 10
+
+for i in range (angka) :
+    print('Angka ke : %d' %(i+1))
+print('\n')
+
+for i in range (angka, 20):
+    print('Angka ke : %d' %(i+1))
+print('\n')
+
+# looping 3 parameter (min, max, lompatan - decre)
+# looping 3 parameter (min, max, lompatan + incre)
+for i in range (angka, 1, -1):
+    print('Angka ke : %d' %(i+1))
+print('\n')
+
+# Array
+array = [1,2,3,4,5]
+
+for i in array :
+    print(i, end = ' ')
