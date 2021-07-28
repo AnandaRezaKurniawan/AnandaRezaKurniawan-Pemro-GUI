008a4715a3547612b45143ba9377c512f92d6687
AnandaRezaKurniawan <72422050+AnandaRezaKurniawan@users.noreply.github.com>
2021-07-28 19:28:07 +0700

    Create DemoIcon.py

---
+++ Praktikum Modul 7/DemoIcon.py
@@ -0,0 +1,31 @@
+import sys
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+## contoh menggunakan icon
+class DemoIcon(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setupUi()
+    def setupUi(self):
+        self.resize(400, 100)
+        self.move(300, 300)
+        self.setWindowTitle('Latihan Icon')
+        self.label = QLabel()
+        self.label.setText('<b>ALAMBUH BODOAMAAT</b>')
+        ## caranya dengan menggunakan QIcon kemudian isikan nama file icon nya
+        icon1 = QIcon('delete-icon.png')
+
+        self.button1 = QPushButton('\tHAPUS SI KUDU')
+        self.button1.setIcon(icon1)
+        layout = QGridLayout()
+        layout.addWidget(self.label, 0, 0 , 1, 5)
+        layout.addWidget(self.button1, 1, 0)
+        self.setLayout(layout)
+
+if __name__ == '__main__':
+    a = QApplication(sys.argv)
+    form = DemoIcon()
+    form.show()
+    a.exec_()
