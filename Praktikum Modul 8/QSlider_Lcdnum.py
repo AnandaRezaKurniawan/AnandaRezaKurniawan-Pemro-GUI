1f89792858e496b160b7c62a31fd30cf5d90e423
AnandaRezaKurniawan <72422050+AnandaRezaKurniawan@users.noreply.github.com>
2021-07-28 19:51:37 +0700

    Create QSlider_Lcdnum.py

---
+++ Praktikum Modul 8/QSlider_Lcdnum.py
@@ -0,0 +1,46 @@
+import sys
+
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+## contoh menggunakan slider
+class MainForm(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setupUi()
+
+
+    def setupUi(self):
+        ## menentukan ukuran widget
+        self.resize(400, 100)
+        self.move(300, 300)
+        self.setWindowTitle('Demo QSlider dan QLCDNumber')
+
+        ## membuat slider dan ukuran/setting nya
+        self.slider = QSlider(Qt.Horizontal)
+        self.slider.setMinimum(-1)
+        self.slider.setMaximum(101)
+        self.slider.setValue(45)
+        self.lcd = QLCDNumber()     # membuat lcd untuk nanti memperjelas nilai slider
+        self.lcd.setDigitCount(3)
+        self.lcd.display(45)
+
+        # merapihkan widget secara vertical
+        layout = QVBoxLayout()
+        layout.addWidget(self.slider)
+        layout.addWidget(self.lcd)
+        self.setLayout(layout)
+        # menghubungkan slider dengan fungsinya
+        self.slider.sliderMoved.connect(self.sliderMoved)
+
+    ## berguna untuk menghubungkan slider dengan lcd ketika digeser
+    def sliderMoved(self):
+        self.lcd.display(str(self.slider.value()))
+
+
+if __name__ == '__main__':
+    a = QApplication(sys.argv)
+    form = MainForm()
+    form.show()
+    a.exec_()
